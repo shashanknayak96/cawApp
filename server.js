93db23d23d056827daff33f762988cb72691fb09
Shashank <shashank.nayak@gmail.com>
2021-05-16 19:45:26 +0530

    first deploy

--- server.js
+++ server.js
@@ -18,8 +18,30 @@ app.use((req, res, next) => {
     next();
 });
 
+const forceSSL = function() {
+    return function (req, res, next) {
+      if (req.headers['x-forwarded-proto'] !== 'https') {
+        return res.redirect(
+         ['https://', req.get('Host'), req.url].join('')
+        );
+      }
+      next();
+    }
+  }// Instruct the app
+  // to use the forceSSL
+  // middlewareapp.use(forceSSL());
+
+  app.use(forceSSL());
+
+
+
 app.use('/user', userRoutes);
 app.use('/caw', cawRoutes);
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
+
 mongoose.set('useFindAndModify', false);
 mongoose.connect("mongodb+srv://shashank:CawCaw@cawapp.tld43.mongodb.net/cawAppDB?retryWrites=true&w=majority", {
     useNewUrlParser: true,
@@ -28,7 +50,7 @@ mongoose.connect("mongodb+srv://shashank:CawCaw@cawapp.tld43.mongodb.net/cawAppD
     .then(
         result => {
             console.log('Mongodb Connected!');
-            app.listen(3000);
+            app.listen(process.env.PORT || 8080);
         }
     )
     .catch(err => console.log(err));
